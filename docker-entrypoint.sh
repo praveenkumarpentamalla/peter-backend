c75c57c64e754993d34abdc717c57c099c791a72
Praveen Kumar Pentamalla <103409229+praveenkumarpentamalla@users.noreply.github.com>
2025-08-14 14:02:50 +0530

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+exec gunicorn -w 4 -b 0.0.0.0:8000 backend.wsgi:application
